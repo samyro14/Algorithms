927c74796b1a608f9ef321d5d1a6392c51ece3a0
samyro14 <72630697+samyro14@users.noreply.github.com>
2023-02-13 23:33:30 +0200

    Update convex_hull.cpp

--- convex_hull.cpp
+++ convex_hull.cpp
@@ -1,66 +1,72 @@
-#include <fstream>
+#include <cstdio>
+
 #include <algorithm>
-#include <cmath>
-#define INF 1002
+#include <fstream>
+#include <iomanip>
+
 using namespace std;
-ifstream fin ("infasuratoareconvexa.in");
-ofstream fout("infasuratoareconvexa.out");
-int det(int X1, int Y1, int X2, int Y2, int X3, int Y3) {
-    return (X2-X1)*(Y3-Y1) - (X3-X1)*(Y2-Y1);
+
+#define x first
+#define y second
+
+typedef pair<double, double> Point;
+
+ifstream fin("infasuratoare.in");
+ofstream fout("infasuratoare.out");
+
+const int MAX_N = 120005;
+
+int n;
+Point v[MAX_N];
+
+Point stack[MAX_N];
+int head;
+
+void read() {
+    fin >> n;
+    for (int i = 1; i <= n; ++i)
+        fin >> v[i].x >> v[i].y;
 }
-int cmp(const pair<int, int> &a, const pair<int, int> &b) {
-    int d = det(0, 0, a.first, a.second, b.first, b.second);
-    if (d != 0)
-        return d > 0;
-    else
-        return a.first*a.first + a.second*a.second > b.first*b.first + b.second*b.second;
+
+inline double cross_product(const Point& A, const Point& B, const Point& C) {
+    return (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
 }
-pair<int, int> v[103], s[103], aux;
-int n, i, j, k, pminim;
+
+inline int cmp(const Point& p1, const Point& p2) {
+    return cross_product(v[1], p1, p2) < 0;
+}
+
+void sort_points() {
+    int pos = 1;
+    for (int i = 2; i <= n; ++i)
+        if (v[i] < v[pos])
+            pos = i;
+    swap(v[1], v[pos]);
+    sort(v + 2, v + n + 1, cmp);
+}
+
+void convex_hull() {
+    sort_points();
+
+    stack[1] = v[1];
+    stack[2] = v[2];
+    head = 2;
+    for (int i = 3; i <= n; ++i) {
+        while (head >= 2 && cross_product(stack[head - 1], stack[head], v[i]) > 0)
+            --head;
+        stack[++head] = v[i];
+    }
+}
+
+void write() {
+    fout << fixed;
+    fout << head << "\n";
+    for (int i = head; i >= 1; --i)
+        fout << setprecision(9) << stack[i].x << " " << stack[i].y << "\n";
+}
 
 int main() {
-    fin>>n;
-    pminim = 0;
-    v[0].first = v[0].second = INF;
-    for (i=1;i<=n;i++) {
-        fin>>v[i].first>>v[i].second;
-        if (v[i].second < v[pminim].second || ( (v[i].second == v[pminim].second) && (v[i].first < v[pminim].first) ))
-            pminim = i;
-    }
-    v[0] = v[pminim];
-    v[pminim] = v[1];
-    v[1] = v[0];
-    for (i=1;i<=n;i++) {
-        v[i].first -= v[0].first;
-        v[i].second -= v[0].second;
-    }
-    sort(v+2, v+n+1, cmp);
-    for (j=3;j<=n;j++)
-        if (det(v[1].first, v[1].second, v[2].first, v[2].second, v[j].first, v[j].second)) {
-            break;
-        }
-    i = 2;
-    j--;
-    while (i < j) {
-        aux = v[i];
-        v[i] = v[j];
-        v[j] = aux;
-        i++;
-        j--;
-    }
-    s[1] = v[1];
-    s[2] = v[2];
-    k = 2;
-    for (i=3;i<=n;i++) {
-        while (k>=2&&det(s[k-1].first,s[k-1].second,s[k].first,s[k].second,v[i].first,
-            v[i].second)<0) {
-            k--;
-        }
-        s[++k] = v[i];
-    }
-    fout<<k<<"\n";
-    for (i=1;i<=k;i++)
-        fout<<s[i].first + v[0].first<<" "<<s[i].second + v[0].second<<"\n";
-    
-    return 0;
+    read();
+    convex_hull();
+    write();
 }
